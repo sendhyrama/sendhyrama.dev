d987174ba995a184de7c6d65b0248df1e4cd4ba9
sendhyrama <sendhyrama@gmail.com>
2023-06-23 11:14:38 +0700

    add deploy setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
@@ -11,23 +10,25 @@ import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
 
 // https://astro.build/config
+import node from "@astrojs/node";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://sendhyrama.dev",
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    mdx({
-      drafts: true,
-      extensions: [".mdx", ".md"],
-      remarkPlugins: [],
-      rehypePlugins: [],
-    }),
-    sitemap(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), mdx({
+    drafts: true,
+    extensions: [".mdx", ".md"],
+    remarkPlugins: [],
+    rehypePlugins: []
+  }), sitemap(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  })],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
