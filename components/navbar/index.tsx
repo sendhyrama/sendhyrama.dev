7aa37250d38547e3711ff01f9d6985abdfe92010
sendhyrama <sendhyrama@gmail.com>
2024-06-16 16:17:23 +0700

    fix: asset url path

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -34,8 +34,7 @@ export function Navbar({ className, ...props }: Props) {
         role="group"
       >
         <Image
-          src="https://lh3.googleusercontent.com/pw/AP1GczNtLwHFqsUTOLrDcLyPi0ALY9hawZEW_UstQDbOz4po4S62sjmQmAzlk1nJrhxTm0M-3NvrLGQt2xZOma3abYVzWbjaWRWhiiPpqsFWzcRw-39NPyXGFWSspdWg_juzXlaL82X3MRzGDMdk83q87sbyHP7LQt4MXuLCC6A6MuOU9mhZasTWL8NsDwQI6IZprrIzv3xZqJ5NcVeSJFWQohqGcBg87XFwstfhyzotJbVwNIHMT7h6gYWjwqB7JPvUW50q2Nx40AW-8hsILnWDjBRlTA9RCzvVfCRnVJ2EUNUyeB8OvTy-d-o_ZkhTKyt_x64zy78GRP--1plzeM6oh9KaqkT2gNAS-sOH34ybIrBw_pP9HaBQ3ENdITxJDPSaPJzcTKctS0WCU9q1iG9vBrA0oMOw2o0iC-2gr8UxABa5gLS0cH6Zv2Oq6CekWeGgxwW4_FQIgaot5jOvXp7xZNvmU5VRWHXf2gYdmXbnhEMtAZumxBs5ghMGVrrnV5EaYNUY3-j7nZ37cPemKbbZumze-qwWvUMzRGj81hoagC9p-QtxnRltB_2Xt9-6W4jxBliCqhu-4p5gT5zQZs79m3F6_O9veClyR8_MzHhOhuWFV9MZJ9BvuYPi-KOVYYzz_TZBIp8EzN6B0oMRrY0xQ7uT2g3s3Cvxp7cMdpJLEKJN3mNmFw0m35cyCQ-wzh6fgAi0o4-DhwJRyYsW0CdzIjnVYmjhW5dVFn48CgwtCtF0T4AjQ73oVVM0zEaeXaC5_KWoYtOMHZ9mhRWUv6JwwTC0fE7ZAElH89zmd3KZawkJQZuzOayrpl_1Hmg7Q5Clv9V4eq-bgG5rYRezOgk7r8QDyoMdj_HbgylA7IdFkkvwgVV0ohuTV9l6HeWuZCTLeg3vBV62e85SgFnqv6ZeXCAl=w512-h512-s-no-gm?authuser=0"
-          // src={imageAvatar}
+          src="https://storage.googleapis.com/bucket-sendhyrama/home.png"
           width={36}
           height={36}
           alt={defaultMetadata.title}
--- components/polaroid/index.tsx
+++ components/polaroid/index.tsx
@@ -27,8 +27,7 @@ export function Polaroid({ className, ...props }: Props) {
     >
       <div className="bg-zinc-500">
         <Image
-          src="https://lh3.googleusercontent.com/pw/AP1GczMTs6W_BP6cOgBnmILz3ZIYKvHs35c8BZJuQ8J4azYDkwWWVs22aZ3QbLrAVj24EaRY00BkjNJGVZmojS9wfwlOW7V11pvFto0yXzrJKVtN36Jc1fQYzgirvwN_QYLZmb5bryMlPhhXe1jgvCnOF8JYQbeThsjjOREvVo2FINID1F5j5F0noDVxm4QQk-uageYZkBX_PEqVYdscsCBGsbDw0WphauOsbb7WST4P__haDZiWOUyRUJx33yk0TgtesQ6pYMWTbH7dbS-UkHkcF98RWOKqvtaGTqmzleN3GykaQ8v8y2SE04PthbkFYTrd79pXHmHiiS023jpVMIHM5Bs6to1pp98SITWOIYadn8pwQRBfP06jWbLVYG8FAe4JHbWJJmfbfd_MEZGfAELXEmWM0vS_ZUXxDmAlYKOUDbOA6PdUIzAE9XfZT6DyQjXCbDeOPQ4YThOTOYecj2nlhecTtXadhMYpqn9DDzPPM5yczbmuq-zgEiTr4v5JqGjB3MnniEG8UrblapjVsB8DSm59hn-Eilgfn5s0qlmMFHDtg0K-YmTQLeQYn233S51Xiv1YetTj0_1HmxfIhowhqds5LSy4t-3kUxpCdgeJbXXDtofV0z0X6PYqeGitFOhicbVqtcHZRh4CFFBq_u40p-mN4MmvKBWC8RoqrzbnkWCM_xrhP03DMQ6HT-IA63yY3J98eZrtDlPpYm1f7tgvYTH2YNvG6B1ygWDXR5FWmco62BxqQ7De4WRFobJfvN0iRAY1DGD4scCOmMyz5CDzZYgKLeReKpUq2urSwETMmI8xqjHKNxxvsO1YJsFuXz0CXMLBY9hVipa7u_yEsOVaBJGQr9Dpbi3yZ01laPBvMsHXTmpeRodhp6yyoyiE12xvqXJvYEdSErpiPeP-iz3pmrvc=w974-h974-s-no-gm?authuser=0"
-          // src={imageCover}
+          src="https://storage.googleapis.com/bucket-sendhyrama/sendhy-with-lil-sister.jpg"
           alt="cover"
           width={500}
           height={500}
